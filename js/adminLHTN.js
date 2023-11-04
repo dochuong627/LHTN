8f13ca3de94f2a5eaac9d489ffb6231374e57843
Do Chuong <do.chuong.627@gmail.com>
2023-11-05 04:41:43 +0700

    update

--- js/adminLHTN.js
+++ js/adminLHTN.js
@@ -53,56 +53,7 @@
     e.preventDefault();
   });
 
-  //Datetime picker
-  $('#id_0').datetimepicker({
-    "allowInputToggle": true,
-    "showClose": true,
-    "showClear": true,
-    "showTodayButton": true,
-    "format": "HH:mm - DD/MM/YYYY",
-  });
-  $('#id_1').datetimepicker({
-    "allowInputToggle": true,
-    "showClose": true,
-    "showClear": true,
-    "showTodayButton": true,
-    "format": "HH:mm - DD/MM/YYYY",
-  });
-  $('#id_2').datetimepicker({
-    "allowInputToggle": true,
-    "showClose": true,
-    "showClear": true,
-    "showTodayButton": true,
-    "format": "HH:mm - DD/MM/YYYY",
-  });
-  $('#id_3').datetimepicker({
-    "allowInputToggle": true,
-    "showClose": true,
-    "showClear": true,
-    "showTodayButton": true,
-    "format": "HH:mm - DD/MM/YYYY",
-  });
-  $('#id_4').datetimepicker({
-    "allowInputToggle": true,
-    "showClose": true,
-    "showClear": true,
-    "showTodayButton": true,
-    "format": "HH:mm - DD/MM/YYYY",
-  });
-  $('#id_5').datetimepicker({
-    "allowInputToggle": true,
-    "showClose": true,
-    "showClear": true,
-    "showTodayButton": true,
-    "format": "HH:mm - DD/MM/YYYY",
-  });
-  $('#id_6').datetimepicker({
-    "allowInputToggle": true,
-    "showClose": true,
-    "showClear": true,
-    "showTodayButton": true,
-    "format": "DD/MM/YYYY",
-  });
+  
 
 
 })(jQuery); // End of use strict
--- js/datatable/datatable.js
+++ js/datatable/datatable.js
@@ -32,5 +32,45 @@
         "searchable": false, 
         "orderable": false
     });
+    $('#DanhSachBaoCaoVanBanDaiHoi').DataTable({
+        pagingType: 'full_numbers',
+        responsive: true,
+        dom: 't<"mt-5"<"left"p><"right"i>>',
+        language: {
+            "info": "Trang _PAGE_ đến _PAGES_",
+            "paginate": {
+                "first":      '<i class="fa fa-solid fa-angles-left"></i>',
+                "last":       '<i class="fa fa-solid fa-angles-right"></i>',
+                "next":       '<i class="fa fa-solid fa-angle-right"></i>',
+                "previous":   '<i class="fa fa-solid fa-angle-left"></i>'
+            },
+        },
+        "pageLength": 7,
+        "searchable": false, 
+        "orderable": false
+    });
+
+    function addFillTable(){
+        document.getElementById("DanhSachBaoCaoVanBanDaiHoi")
+            .classList
+            .add("group");
+        document.querySelector(".listDocs").classList.remove('bg-gray-200');
+        document.querySelector(".groupDocs").classList.add('bg-gray-200');
+    }
+
+    function removeFillTable(){
+        document.getElementById("DanhSachBaoCaoVanBanDaiHoi").classList.remove("group");
+        
+        document.querySelector(".groupDocs").classList.remove('bg-gray-200');
+        document.querySelector(".listDocs").classList.add('bg-gray-200');
+    }
+
+    $('.groupDocs').on("click",function(){
+        addFillTable()
+    })
+
+    $('.listDocs').on("click",function(){
+        removeFillTable()
+    })
     
 })(jQuery); // End of use strict
